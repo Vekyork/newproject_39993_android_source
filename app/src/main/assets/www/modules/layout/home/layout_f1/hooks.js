662d5667c4c40c2072950ccaee031e184cfaf7de
Vekyork <vekyork@yandex.ru>
2022-03-24 23:26:04 +0300

    Pictures

---
+++ app/src/main/assets/www/modules/layout/home/layout_f1/hooks.js
@@ -0,0 +1,59 @@
+App.service("layout_f1", function ($rootScope, HomepageLayout) {
+
+    var service = {};
+
+    /**
+     * Must return a valid template
+     *
+     * @returns {string}
+     */
+    service.getTemplate = function() {
+        return "modules/layout/home/layout_f1/view.html";
+    };
+
+    /**
+     * Must return a valid template
+     *
+     * @returns {string}
+     */
+    service.getModalTemplate = function() {
+        return "templates/home/l10/modal.html";
+    };
+
+    /**
+     * onResize is used for css/js callbacks when orientation change
+     */
+    service.onResize = function() {
+        /** Do nothing for this particular one */
+    };
+
+    /**
+     * Manipulate the features objects
+     *
+     * Examples:
+     * - you can re-order features
+     * - you can push/place the "more_button"
+     *
+     * @param features
+     * @param more_button
+     * @returns {*}
+     */
+    service.features = function(features, more_button) {
+        /** Place more button at the end */
+        /**features.overview.options.push(more_button);*/
+        return features;
+    };
+
+    return service;
+
+});
+
+App.controller("F1Controller", function($scope)
+{   
+    $scope.DisplayMenu = function () {
+        var main_menu = document.getElementById("main_menu");
+        main_menu.classList.toggle("active");
+        var menu_btn = document.getElementById("menu_btn");
+        menu_btn.classList.toggle("active");
+    }
+});
